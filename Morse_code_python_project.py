0b30175ce873a8de2232933f6d36905337a73b25
Mansi Salar <155888395+Mansi06Salar@users.noreply.github.com>
2024-01-08 18:39:44 +0530

    Add files via upload

---
+++ Morse_code_python_project.py
@@ -0,0 +1,19 @@
+import re
+s = "python"
+
+def morse(text):
+     encrypt = {
+         'A':'.-','B':'-...',
+'C':'-.-.', 'D':'-..', 'E':'.',
+'F':'..-.', 'G':'--.', 'H':'....',
+'I':'..', 'J':'.---', 'K':'-.-',
+'L':'.-..', 'M':'--', 'N':'-.',
+'O':'---', 'P':'.--.', 'Q':'--.-',
+'R':'.-.', 'S':'...', 'T':'-',
+'U':'..-', 'V':'...-', 'W':'.--',
+'X':'-..-', 'Y':'-.--', 'Z':'--..', ' ':'.....'
+     }
+     decrypt = {value: key for key,value in encrypt.items()}
+     if re.match ('(\s|-|\.)+', text):
+         return ''.join(decrypt[i] for i in text.split())
+     return ' '.join(encrypt[i] for i in text.upper())
